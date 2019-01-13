2c485ba05acbcd65e796619d290540e7fd41c2ca
Clemens Schneider <clemens.schneider@posteo.de>
2019-01-13 14:40:38 +0100

    update

--- linq.cs
+++ linq.cs
@@ -52,3 +52,27 @@ int product = intCollection.Aggregate((a, b) => a * b); // build product of enti
 // Where function (restrictions):
 
 IEnumerable<int> evenNumbers = intCollection.Where(number => number % 2 == 0);
+
+IEnumerable<int> evenNumber = intCollection.Where((number, index) => index > 4 && number % 2 == 0); // considering index
+
+// Projection functions:
+public class Employee
+{
+    public Employee(int ID, string Name)
+    {
+        this.ID = ID;
+        this.Name = Name;
+    }
+
+    public int ID { get; set; }
+    public string Name { get; set; }
+}
+
+List<Employee> employees = new List<Employee>
+{
+    new Employee(1, "Heinz"),
+    new Employee(2, "Sabine")
+};
+
+IEnumerable<string> employeeNames = employees.Select(x => x.Name); // Select only name column
+
