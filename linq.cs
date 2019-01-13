4f97c356f59b31440ac056713cc2c8178649d3dc
Clemens Schneider <clemens.schneider@posteo.de>
2019-01-13 16:19:38 +0100

    update

--- linq.cs
+++ linq.cs
@@ -56,12 +56,15 @@ IEnumerable<int> evenNumbers = intCollection.Where(number => number % 2 == 0);
 IEnumerable<int> evenNumber = intCollection.Where((number, index) => index > 4 && number % 2 == 0); // considering index
 
 // Projection functions:
+
+// Select
 public class Employee
 {
-    public Employee(int ID, string Name)
+    public Employee(int ID, string Name, int Salary)
     {
         this.ID = ID;
         this.Name = Name;
+        this.Salary = Salary;
     }
 
     public int ID { get; set; }
@@ -71,11 +74,34 @@ public class Employee
 
 List<Employee> employees = new List<Employee>
 {
-    new Employee(1, "Heinz"),
-    new Employee(2, "Sabine")
+    new Employee(1, "Heinz", 1000),
+    new Employee(2, "Sabine", 2000)
 };
 
-IEnumerable<string> employeeNames = employees.Select(x => x.Name); // Select only name column
+IEnumerable<string> employeeNames = employees.Select(employee => employee.Name); // Select only name column
 
-var employeeNames = employees.Select(x => new { ID = x.ID, Name = x.Name }); // Select multiple columns with anonymous type
+var employeeNames = employees.Select(employee => new { employee.ID, employee.Name }); // Select multiple columns with anonymous type
 
+//SelectMany vs Select
+public class Employee
+{
+    public Employee(int ID, string Name, string[] Hobbies)
+    {
+        this.ID = ID;
+        this.Name = Name;
+        this.Hobbies = Hobbies;
+    }
+
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string[] Hobbies { get; set; }
+}
+
+List<Employee> employees = new List<Employee>
+{
+    new Employee(1, "Heinz", new string[]{"Football", "TV" }),
+    new Employee(2, "Sabine", new string[]{"Gardening", "Traveling" })
+};
+
+var employeesHobbies1 = employees.Select(employee => new { employee.Name, employee.Hobbies }); // 2 rows
+var employeesHobbies2 = employees.SelectMany(employee => employee.Hobbies, (employee, hobby)  => new { employee.Name, hobby }); // 4 rows
