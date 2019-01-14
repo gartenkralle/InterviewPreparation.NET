79a7988e45d220caa3a56e51c83f3af2ccbc8870
Clemens Schneider <clemens.schneider@posteo.de>
2019-01-14 18:01:03 +0100

    update

--- ado net.cs
+++ ado net.cs
@@ -5,26 +5,39 @@
 //Full capabilities of sql server
 
 //Disadvantages:
-//SqlCommand is prone to sql injection
-//No intellisens for command string
-//Typos result in runtime errors, not compile time errors
+//SqlCommand is prone to sql injection (on client side)
+//No intellisense for command string (on client side)
+//Typos result in runtime errors, not compile time errors (on client side)
 //Transfering SqlCommand to sql server generates more network traffic than stored procedures or functions
 
 //First Example
 protected void Page_Load(object sender, EventArgs e)
 {
-    SqlConnection sqlConnection = new SqlConnection("data source=.; database=Sample; integrated security=SSPI");
-    SqlCommand sqlCommand = new SqlCommand("Select * From Employee", sqlConnection);
+    string connectionString = "data source=.; database=Sample; integrated security=SSPI";
 
-    sqlConnection.Open();
-    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+    using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+    {
+        SqlCommand sqlCommand = new SqlCommand("Select * From Employee", sqlConnection);
 
-    GridView1.DataSource = sqlDataReader;
-    GridView1.DataBind();
+        sqlConnection.Open();
+        SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
 
-    sqlConnection.Close();
+        GridView1.DataSource = sqlDataReader;
+        GridView1.DataBind();
+    }// automatically calls sqlConnection.Close();
 }
 
 //Typically store connection string in web.config or app.config and access with:
 string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+
+
+//ExecuteReader (rows of data will be returned)
+SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+//ExecuteScalar (single value will be returned)
+int rowCount = (int)sqlCommand.ExecuteScalar();
+
+//ExecuteNonQuery (single value will be returned which indicate the effected rows)
+//Insert, Update, Delete
+int affectedRowsCount = sqlCommand.ExecuteNonQuery();
 
