c3bf1157ab6ffeae8372c53db5a04b168f01b448
Clemens Schneider <clemens.schneider@posteo.de>
2019-01-14 16:59:35 +0100

    update

--- ado net.cs
+++ ado net.cs
@@ -1,8 +1,16 @@
 //ADO.NET
 //Abstraction layer to interact with data source like database or xml file.
 
-//First Example
+//Advantages:
+//Full capabilities of sql server
 
+//Disadvantages:
+//SqlCommand is prone to sql injection
+//No intellisens for command string
+//Typos result in runtime errors, not compile time errors
+//Transfering SqlCommand to sql server generates more network traffic than stored procedures or functions
+
+//First Example
 protected void Page_Load(object sender, EventArgs e)
 {
     SqlConnection sqlConnection = new SqlConnection("data source=.; database=Sample; integrated security=SSPI");
@@ -16,4 +24,7 @@ protected void Page_Load(object sender, EventArgs e)
 
     sqlConnection.Close();
 }
+
+//Typically store connection string in web.config or app.config and access with:
+string connectionString = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
 
